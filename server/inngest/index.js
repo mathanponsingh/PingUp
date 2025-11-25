410e3edf46fc8212300d21b199c1bb1f46b03040
Mathan Ponsingh <mathanponsingh@gmail.com>
2025-11-25 19:20:40 +0530

    email reminder

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEamil from "../configs/nodeMailer.js";
-import { connection } from "mongoose";
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
